939a8a6da7e8c62432cfbf5c4153e25b7b7228d5
Gabe Reichenberger <gabe.reichenberger@gmail.com>
2024-08-30 00:47:33 -0700

    minor formatting

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,11 @@ import styles from "./app.module.css";
 import CircleCanvas from "./components/canvascomponent/CircleCanvas";
 
 function App() {
-  
-
   return (
     <div className={styles.app}>
       <h1>Animated Circle Dots on Canvas</h1>
       <div className={styles.canvasContainer}>
-        <CircleCanvas
-        />
+        <CircleCanvas/>
       </div>   
     </div>
   );
